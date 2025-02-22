19d11a6573c21b7f07c862bfd0e3b330acd50774
Asunm285b22106 <144204553+Asunm285b22106@users.noreply.github.com>
2025-02-22 23:55:28 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+</head>
+<body>
+    <form method="get" action="action.php">
+    <center>
+        <div><center><h1>LOGIN</h1></center></div>
+        
+        <label>First Name</label>
+        <input type="text" name="firstname"><br><br>
+        
+        <label>Last  Name</label>
+        <input type="text" name="lastname"><br><br>
+        
+        <input type="submit" name="submit">
+    </center>
+</body>
+</html>
